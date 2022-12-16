b1a2ecf765d5c0dd2a2ef3ba82f613f5521317c3
Darien Sandifer <dariens@duck.com>
2022-12-16 12:16:20 -0600

    added searchbar

---
+++ Hush/SeachBar.swift
@@ -0,0 +1,51 @@
+//
+//  SeachBar.swift
+//  Hush
+//
+//  Created by Darien Sandifer on 12/16/22.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    
+    @Binding var searchText: String
+    @Binding var searching: Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color("LightGray"))
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("Search ..", text: $searchText) { startedEditing in
+                    if startedEditing {
+                        withAnimation {
+                            searching = true
+                        }
+                    }
+                } onCommit: {
+                    withAnimation {
+                        searching = false
+                    }
+                }
+            }
+            .foregroundColor(.gray)
+            .padding(.leading, 13)
+        }
+        .frame(height: 40)
+        .cornerRadius(13)
+    }
+}
+
+
+//extension UIApplication {
+//     func dismissKeyboard() {
+//         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//     }
+// }
+struct SeachBar_Previews: PreviewProvider {
+    static var previews: some View {
+      SearchBar(searchText: .constant(""), searching: .constant(true))
+    }
+}
